9672ce920f307cc6fc100cbedb0e8eff0b5bb41d
Raghav-0007 <rmyadav514@gmail.com>
2025-01-12 15:18:45 +0530

    Added LargestNumberCheck.java

---
+++ Day-2_ControlFlow/Level1-PracticeProblem/LargestNumberCheck.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class LargestNumberCheck {
+    public static void main(String[] args) {
+        // Create a Scanner object to take user input
+        Scanner input = new Scanner(System.in);
+
+        // Taking numbers from user
+        System.out.print("Enter the first number: ");
+        int number1 = input.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int number2 = input.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int number3 = input.nextInt();
+
+        // Check which number is the largest
+        boolean isFirstLargest = (number1 > number2) && (number1 > number3);
+        boolean isSecondLargest = (number2 > number1) && (number2 > number3);
+        boolean isThirdLargest = (number3 > number1) && (number3 > number2);
+
+        // Print the results
+        System.out.println("Is the first number the largest? " + isFirstLargest);
+        System.out.println("Is the second number the largest? " + isSecondLargest);
+        System.out.println("Is the third number the largest? " + isThirdLargest);
+
+        // Close the Scanner object
+        input.close();
+    }
+}
