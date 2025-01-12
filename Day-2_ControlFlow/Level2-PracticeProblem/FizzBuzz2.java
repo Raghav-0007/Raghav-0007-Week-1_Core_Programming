c06a4b5fb747ab814bc1b64b7fae43698b95ae5e
Raghav-0007 <rmyadav514@gmail.com>
2025-01-12 15:22:39 +0530

    Added FizzBuzz2.java

---
+++ Day-2_ControlFlow/Level2-PracticeProblem/FizzBuzz2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class FizzBuzz2 {
+    public static void main(String[] args) {
+        // Create a scanner object for taking user input
+        Scanner input = new Scanner(System.in);
+        
+        // Take the number from the user
+        System.out.print("Enter a positive integer: ");
+        int number = input.nextInt();
+        
+        // Check if the number is positive
+        if (number > 0) {
+			int i=0;
+            
+            while (i <= number) {
+                // Check for multiples of 3 and 5
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                }
+                // Check for multiples of 3
+                else if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                }
+                // Check for multiples of 5
+                else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                }
+                // For other numbers, print the number itself
+                else {
+                    System.out.println(i);
+                }
+				i++;
+            }
+        } else {
+            System.out.println("Please enter a positive integer.");
+        }
+        
+        // Close the scanner object
+        input.close();
+    }
+}
