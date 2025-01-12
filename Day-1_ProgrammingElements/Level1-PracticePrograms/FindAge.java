d786f997f21161095183751243237a5af381502d
Raghav-0007 <rmyadav514@gmail.com>
2025-01-12 15:10:07 +0530

    Added FindAge.java

---
+++ Day-1_ProgrammingElements/Level1-PracticePrograms/FindAge.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FindAge{
+public static void main(String[] args){
+	
+// creat an scanner class object for taking input
+Scanner sc = new Scanner(System.in);
+
+// calculate harry's age
+int ageOfHarry=2024-2000;
+
+// print harry's age
+System.out.println("Harry's age in 2024 is: " + ageOfHarry);
+
+// close the scanner class object
+sc.close();
+}
+}
