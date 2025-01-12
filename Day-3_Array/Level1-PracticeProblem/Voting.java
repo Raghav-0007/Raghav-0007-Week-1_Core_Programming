e2f8b1341decdf90e2781592803c6aee1261e534
Raghav-0007 <rmyadav514@gmail.com>
2025-01-12 15:27:34 +0530

    Added Voting.java

---
+++ Day-3_Array/Level1-PracticeProblem/Voting.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Voting{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Array to store the age of 10 students
+        int[] ages = new int[10];
+
+        // Input ages of 10 students
+        for (int i = 0; i < ages.length; i++) {
+            System.out.print("Enter the age of student " + (i + 1) + ": ");
+            ages[i] = scanner.nextInt();
+        }
+
+        // Check voting eligibility
+        for (int i = 0; i < ages.length; i++) {
+            int age = ages[i];
+
+            if (age < 0) {
+                System.out.println("Invalid age entered for student " + (i + 1));
+            } else if (age >= 18) {
+                System.out.println("The student with the age " + age + " can vote.");
+            } else {
+                System.out.println("The student with the age " + age + " cannot vote.");
+            }
+        }
+
+        scanner.close();
+    }
+}
