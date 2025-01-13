27f3badb14bb1d5f9eb2861a387927d85f2ea0c4
Raghav-0007 <rmyadav514@gmail.com>
2025-01-13 13:57:21 +0530

    Added FactorCalculator.java

---
+++ Day-4_Methods/Level2-PracticeProblem/FactorCalculator.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class FactorCalculator {
+
+    // Method to find factors of a number
+    public static int[] findFactors(int number) {
+        int count = 0;
+        // First loop to count factors
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+        int[] factors = new int[count];
+        int index = 0;
+        // Second loop to store factors in array
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                factors[index++] = i;
+            }
+        }
+        return factors;
+    }
+
+    // Method to find the sum of factors
+    public static int sumOfFactors(int[] factors) {
+        int sum = 0;
+        for (int factor : factors) {
+            sum += factor;
+        }
+        return sum;
+    }
+
+    // Method to find the product of factors
+    public static int productOfFactors(int[] factors) {
+        int product = 1;
+        for (int factor : factors) {
+            product *= factor;
+        }
+        return product;
+    }
+
+    // Method to find the sum of squares of factors
+    public static int sumOfSquaresOfFactors(int[] factors) {
+        int sum = 0;
+        for (int factor : factors) {
+            sum += Math.pow(factor, 2);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int[] factors = findFactors(number);
+        System.out.print("Factors: ");
+        for (int factor : factors) {
+            System.out.print(factor + " ");
+        }
+        System.out.println("\nSum of factors: " + sumOfFactors(factors));
+        System.out.println("Product of factors: " + productOfFactors(factors));
+        System.out.println("Sum of squares of factors: " + sumOfSquaresOfFactors(factors));
+
+        scanner.close();
+    }
+}
