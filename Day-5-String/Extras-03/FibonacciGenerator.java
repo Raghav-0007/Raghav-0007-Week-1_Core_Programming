f085308dd8b063a6c8854b90dac16b5a005bce85
Raghav-0007 <rmyadav514@gmail.com>
2025-01-20 22:44:16 +0530

    Added FibonacciGenerator.java

---
+++ Day-5-String/Extras-03/FibonacciGenerator.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class FibonacciGenerator {
+
+    // Function to generate and print the Fibonacci sequence
+    static void generateFibonacci(int terms) {
+        int first = 0, second = 1;
+        
+        System.out.print("Fibonacci Sequence: ");
+        for (int i = 1; i <= terms; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of Fibonacci terms: ");
+        int numTerms = scanner.nextInt();
+
+        if (numTerms <= 0) {
+            System.out.println("Please enter a positive number.");
+        } else {
+            generateFibonacci(numTerms);
+        }
+
+        scanner.close();
+    }
+}
+
